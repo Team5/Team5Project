362418c754715e7703692162987dcacca26a4014
kevin <kevin.kevinleo@gmail.com>
2011-02-20 15:22:57 +0000

    rooms table schema updated to make more sense (can now sort by various facilities) rooms view updated to show new fields

--- application/views/collection/rooms.php
+++ application/views/collection/rooms.php
@@ -4,15 +4,27 @@
         <tr>
             <th>selected</th>
             <th>rid</th>
-            <th>title</th>
-            <th>facilities</th>
+            <th>name</th>
+            <th>building</th>
+            <th>chairs</th>
+            <th>computers</th>
+            <th>projector</th>
+            <th>wheelchair</th>
+            <th>printers</th>
+            <th>wireless</th>
         </tr>
         <? if(count($rooms)>0): $odd=0; foreach($rooms as $room):?>
         <tr<?=($odd^=1)?' class="odd_row"':''?>>
             <td><input name="selected_tbl[]" value="rooms" id="checkbox_tbl_1" type="checkbox"/></td>
-            <td><?=$room->room_id?></td>
-            <td><?=$room->title?></td>
-            <td><?=$room->facilities?></td>
+            <td><?=$room->rid?></td>
+            <td><?=$room->name?></td>
+            <td><?=$room->building?></td>
+            <td><?=$room->chairs?></td>
+            <td><?=$room->computers?></td>
+            <td><?=$room->projector?></td>
+            <td><?=$room->wheelchair?></td>
+            <td><?=$room->printer?></td>
+            <td><?=$room->wireless?></td>
         </tr>
         <? endforeach; else:?>
            <td colspan="all">Nothing here</td>
