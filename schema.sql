a278f2fd1c99afd7a94e0aca1bfd65ed16f43545
kevin <kevin.kevinleo@gmail.com>
2011-03-07 12:48:12 +0000

    Added updated schema

--- schema.sql
+++ schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `courses` (
 
 INSERT INTO `courses` (`enrolled_max`, `cid`, `pid`, `rid`, `area`, `title`, `description`, `price`, `start_date`, `end_date`, `enrolled_count`) VALUES
 (30, 1, 4, 2, 'science', 'Course One', 'This is the description for course ONE! \r\nThis is the description for course ONE!\r\nThis is the description for course ONE!\r\nThis is the description for course ONE!\r\nThis is the description for course ONE!\r\nThis is the description for course ONE!\r\nThis is the description for course ONE!\r\nThis is the description for course ONE!\r\nThis is the description for course ONE!', 2000, '2011-02-15', '2011-02-24', 1),
-(30, 2, 5, 1, 'arts', 'Course Two for Bleh', 'Course two info! Course two info!\r\nCourse two info!\r\nCourse two info!\r\nCourse two info!\r\nCourse two info!Course two info!Course two info!\r\nCourse two info!', 2000, '2011-02-09', '2011-02-24', 32),
+(30, 2, 5, 1, 'arts', 'Course Two for Bleh', 'Course two info! Course two info!\r\nCourse two info!\r\nCourse two info!\r\nCourse two info!\r\nCourse two info!Course two info!Course two info!\r\nCourse two info!', 2000, '2011-02-09', '2011-02-24', 30),
 (30, 3, 5, 2, 'arts', 'French', 'Blah blah blah\r\n\r\n\r\nFrench \r\nClass\r\n', 3000, '2011-02-16', '2011-02-24', 20),
 (30, 4, 4, 2, 'science', 'Course about sciency stuff', 'This course is about sciency stuff', 1550, '2011-02-01', '2011-02-15', 3),
 (30, 5, 5, 1, 'arts', 'Spanish Classes', 'You can learn spanish here, maybe.\r\nYou can learn spanish here, maybe.\r\nYou can learn spanish here, maybe.\r\nYou can learn spanish here, maybe.\r\nYou can learn spanish here, maybe.\r\nYou can learn spanish here, maybe.\r\nYou can learn spanish here, maybe.\r\nYou can learn spanish here, maybe.\r\nYou can learn spanish here, maybe.\r\nMaybe, Maybe, Maybe, Maybe, Maybe.\r\nOooh it lined up', 2000, '2011-02-10', '2011-02-23', 5),
@@ -38,28 +38,34 @@ INSERT INTO `courses` (`enrolled_max`, `cid`, `pid`, `rid`, `area`, `title`, `de
 CREATE TABLE IF NOT EXISTS `enrollment` (
   `uid` int(11) NOT NULL,
   `cid` int(11) NOT NULL,
-  `enrolled` tinyint(1) NOT NULL
+  `enrolled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`uid`,`cid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `enrollment` (`uid`, `cid`, `enrolled`) VALUES
 (1, 1, 0),
+(1, 2, 1),
 (1, 4, 1),
-(1, 11, 1),
-(1, 2, 0),
 (1, 5, 0),
+(1, 8, 1),
+(1, 11, 1),
 (1, 15, 1),
-(2, 1, 1);
+(2, 1, 1),
+(3, 1, 1),
+(3, 6, 1),
+(6, 3, 0),
+(6, 9, 1);
 
 CREATE TABLE IF NOT EXISTS `news` (
-  `article_id` int(11) NOT NULL AUTO_INCREMENT,
-  `provider_id` int(11) NOT NULL,
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL,
   `title` varchar(32) NOT NULL,
   `article_text` text NOT NULL,
   `publish_datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`article_id`)
+  PRIMARY KEY (`aid`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
-INSERT INTO `news` (`article_id`, `provider_id`, `title`, `article_text`, `publish_datetime`) VALUES
+INSERT INTO `news` (`aid`, `pid`, `title`, `article_text`, `publish_datetime`) VALUES
 (1, 1, 'News about course 1', 'Blahdya asgasg\r\nBlahdya asgasgBlahdya asgasg\r\n\r\nBlahdya asgasg\r\n\r\n\r\n\r\nBlahdya asgasgBlahdya asgasgBlahdya asgasg\r\n\r\nBlahdya asgasgBlahdya asgasgBlahdya asgasg\r\n\r\nBlahdya asgasgBlahdya asgasg', '2011-02-05 16:49:42'),
 (2, 2, 'Infor about 24', 'agasg asg asg asg asg \r\nagasg asg asg asg asg agasg asg asg asg asg \r\n\r\n\r\nagasg asg asg asg asg agasg asg asg asg asg \r\n\r\nagasg asg asg asg asg agasg asg asg asg asg agasg asg asg asg asg agasg asg asg asg asg agasg asg asg asg asg ', '2011-02-05 16:50:02');
 
@@ -89,17 +95,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `fname` varchar(32) NOT NULL,
   `sname` varchar(32) NOT NULL,
   `date_of_birth` date NOT NULL,
+  `validate_string` varchar(64) NOT NULL,
+  `validated` tinyint(1) NOT NULL,
   PRIMARY KEY (`uid`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
-INSERT INTO `users` (`uid`, `type`, `area`, `email`, `password`, `fname`, `sname`, `date_of_birth`) VALUES
-(1, 'user', 'general', 'u1@k.com', '20a0db53bc1881a7f739cd956b740039', 'User One', '', '2011-02-14'),
-(2, 'user', 'general', 'u2@k.com', '1926f73f97bf1985b2b367730cb75071', 'User Two', '', '2011-02-14'),
-(3, 'user', 'general', 'u3@k.com', '2a9c9eb70d17fa6bf0e5cc1bc99a339d', 'User 3', '', '2011-02-06'),
-(4, 'provider', 'science', 'p1@p.com', 'e35540f334ffc6042ccfff9579e8be20', 'Provider One', '', '2011-02-13'),
-(5, 'provider', 'languages', 'p2@p.com', '5cc55c68d8817fe01b7af8aa9871f641', 'Provider Two', '', '2011-02-27'),
-(6, 'user', 'general', 'u4@k.com', 'ddc6f7b6b52b7bd1f4b77e31d2ab4213', 'User Four', '', '0000-00-00'),
-(7, 'user', 'general', 'john@doe.com', '6579e96f76baa00787a28653876c6127', 'John Doe', '', '0000-00-00'),
-(0, 'admin', 'admin', 'admin@sc.ucc.ie', '25e4ee4e9229397b6b17776bfceaf8e7', 'admin', '', '1986-01-01'),
-(13, 'user', 'general', 'kevin.kevinleo@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Kevin', 'Leo', '1990-03-18');
+INSERT INTO `users` (`uid`, `type`, `area`, `email`, `password`, `fname`, `sname`, `date_of_birth`, `validate_string`, `validated`) VALUES
+(1, 'user', 'general', 'u1@k.com', '20a0db53bc1881a7f739cd956b740039', 'User One', '', '2011-02-14', '', 0),
+(2, 'user', 'general', 'u2@k.com', '1926f73f97bf1985b2b367730cb75071', 'User Two', '', '2011-02-14', '', 0),
+(3, 'user', 'general', 'u3@k.com', '2a9c9eb70d17fa6bf0e5cc1bc99a339d', 'User 3', '', '2011-02-06', '', 0),
+(4, 'provider', 'science', 'p1@p.com', 'e35540f334ffc6042ccfff9579e8be20', 'Provider One', '', '2011-02-13', '', 0),
+(5, 'provider', 'languages', 'p2@p.com', '5cc55c68d8817fe01b7af8aa9871f641', 'Provider Two', '', '2011-02-27', '', 0),
+(6, 'user', 'general', 'u4@k.com', 'ddc6f7b6b52b7bd1f4b77e31d2ab4213', 'User Four', '', '0000-00-00', '', 0),
+(7, 'user', 'general', 'john@doe.com', '6579e96f76baa00787a28653876c6127', 'John Doe', '', '0000-00-00', '', 0),
+(0, 'admin', 'admin', 'admin@sc.ucc.ie', '25e4ee4e9229397b6b17776bfceaf8e7', 'admin', '', '1986-01-01', '', 0),
+(13, 'user', 'general', 'kevin.kevinleo@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'Kevin', 'Leo', '1990-03-18', '', 0),
+(14, 'user', 'general', 's@d.com', '5f4dcc3b5aa765d61d8327deb882cf99', 'someonw', 'else', '0000-00-00', '', 0);
